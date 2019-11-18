9046c7e3e98b9f6d8e7fa47f2c446327ec9c4a57
Griffin <bluedogerino@gmail.com>
2019-11-18 17:03:42 +0100

    LO3 [1/2]

---
+++ L03/trougao/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,s;
+    printf("Unesite stranicu a : ");
+    scanf("%d", &a);
+    printf("Unesite stranicu b : ");
+    scanf("%d", &b);
+    printf("Unesite stranicu c : ");
+    scanf("%d", &c);
+    printf("Trougao je : ");
+    double povrsina,obim;
+    s=((a+b+c)/2);
+    povrsina=(pow(((s*(s-a)*(s-b)*(s-c))),0.5));
+    obim=(a+b+c);
+    if ((a+b)>c)
+    {
+        if ((b+c)>a)
+        {
+            if((a+c)>b)
+            {
+                if(a==b && b==c && c==a)
+                {
+                    printf("Jednakostranicni trougao\n");
+                    if((a*a==b*b+c*c)||(b*b==a*a+c*c)||(c*c==a*a+b*b))
+                    {
+                        printf("Trougao je pravougli\n");
+                        printf("Povrsina : %.2f", povrsina);
+                        printf("Obim : %.2f", obim);
+                    }
+                }
+                else if(a==b || b==c)
+                {
+                    printf("Jednakokraki trougao\n");
+                    if((a*a==b*b+c*c)||(b*b==a*a+c*c)||(c*c==a*a+b*b))
+                    {
+                        printf("Trougao je pravougli\n");
+                        printf("Povrsina : %.2f", povrsina);
+                        printf("Obim : %.2f", obim);
+                    }
+                }
+                else if(a!=b!=c)
+                {
+                    printf("Raznostranicni trougao\n");
+                    if((a*a==b*b+c*c)||(b*b==a*a+c*c)||(c*c==a*a+b*b))
+                    {
+                        printf("Trougao je pravougli\n");
+                        printf("Povrsina : %.2f \n", povrsina);
+                        printf("Obim : %.2f \n", obim);
+                    }
+                }
+
+
+            }
+
+                else
+                {
+                    printf("Trougao se ne moze konstruisati.\n");
+                }
+        }
+        else
+            {
+                printf("Trougao se ne moze konstruisati.\n");
+            }
+    }
+    return 0;
+}
